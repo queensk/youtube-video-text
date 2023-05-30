0c5e2c7267cfa2c91ca4437827eb763b05e863e6
queensk <queenskisivuli@gmail.com>
2023-05-30 21:43:45 +0300

    youtube-transcript-api function

--- api/index.py
+++ api/index.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import requests
+from youtube_transcript_api import YouTubeTranscriptApi
 
 app = FastAPI()
 
 @app.get("/api/python")
 def hello_world():
     return {"message": "Hello World"}
+
+class Video(BaseModel):
+    url: str
+
+@app.post("/api/text")
+def get_video_id(video: Video):
+    """
+    assuming the url is in the format https://www.youtube.com/watch?v=video_id
+    """
+    video_id = video.url.split('=')[-1]
+    transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    text = ""
+    for item in transcript:
+        text += item['text'] + " "
+    
+    return {"text": text}
